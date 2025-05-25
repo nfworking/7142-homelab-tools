6966e5cca4ec84d10743829f62d6d8ab0679fea2
nfworking <tidalfarmer1432@outlook.com>
2025-05-25 19:40:46 +1000

    updated produce to reflect prev changes

--- produce.ps1
+++ produce.ps1
@@ -1,5 +1,6 @@
 param(
     [string]$PreCon = ".\PreReqChk\PreReqChk.csproj",
+    [string]$PregChkPreview = ".\PreReqChkPreview\PreReqChkPreview.csproj",
     [string]$PreSel = ".\PreReqChkSel\PreReqChkSel.csproj",
     [string]$PreExMoe = ".\moeDEP\moeDEP.csproj",
     [string]$PreExPor = ".\mobDEP\mobDEP.csproj",
@@ -7,6 +8,8 @@ param(
     [string]$PreSelOut = "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreSel",
     [string]$PreExMoeOut = "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreExMoe",
     [string]$PreExPorOut = "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreExPor",
+    [string]$PreReqChkPreOut = "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreChkPreview",
+    [string]$MasterFolder = "C:\users\tidal\projects\7142_Joiner_Tools\master_dist",
     [string]$Runtime = "win-x64"
 )
 
@@ -46,3 +49,24 @@ dotnet publish $PreExPor `
     -p:PublishSingleFile=true `
     -p:PublishTrimmed=false `
     -o $PreExPorOut
+    
+Write-Host("Compiling $PregChkPreview")
+dotnet publish $PregChkPreview `
+    -c Release `
+    -r $Runtime `
+    --self-contained `
+    -p:PublishSingleFile=true `
+    -p:PublishTrimmed=false `
+    -o $PreReqChkPreOut
+     
+Write-Host("Exporting Projects to Master folder")
+xcopy "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreChkPreview" $MasterFolder /Y
+xcopy "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreCon" $MasterFolder /Y
+xcopy "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreSel" $MasterFolder /Y
+xcopy "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreExMoe" $MasterFolder /Y
+xcopy "C:\users\tidal\projects\7142_Joiner_Tools\dist\PreExPor" $MasterFolder /Y
+
+Write-Host("Copying master installer file")
+xcopy masterinst.bat $MasterFolder /Y
+
+
